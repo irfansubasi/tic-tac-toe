ee345e2632b26bda17205ed35d24b3ff4678cd2c
irfansubasi <136987051+irfansubasi@users.noreply.github.com>
2024-04-15 18:22:20 +0300

    add comment lines & arrange codes

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Selecting DOM elements
 const board = document.querySelector("#board");
 const cells = document.querySelectorAll(".cell");
 const aiButton = document.getElementById("player2-ai");
@@ -10,15 +11,19 @@ const dialog = document.querySelector("dialog");
 const restartButton = document.getElementById("restart-btn");
 const aiDiffButton = document.querySelector(".diff");
 
-
+// Constants for player classes
 const X_CLASS = "X";
 const O_CLASS = "O";
+
+// Variables to track scores and current player
 let xScore = 0;
 let oScore = 0;
 let currentPlayer = X_CLASS;
 
+// Boolean to determine if the opponent is AI
 let isOpponentAi = true;
 
+// Winning combinations
 const winningCombos = [
     [0, 1, 2],
     [3, 4, 5],
@@ -30,61 +35,83 @@ const winningCombos = [
     [2, 4, 6]
 ];
 
+// AI difficulty levels
 const Difficulty = {
     EASY: "easy",
     UNBEATABLE: "unbeatable"
 };
 
+// Default difficulty level
 let currentDifficulty = Difficulty.EASY;
 
-
+// Function to start the game
 function startGame(){
 
+    // If it's AI's turn, let AI make a move
     if(currentPlayer == O_CLASS && isOpponentAi){
         aiMove();
     }
-
+    // Add event listeners to cells
     cells.forEach(cell => {
         cell.addEventListener("click", handleClickCell);
     })
 
+    // Show game section and hide start screen
     game.style.display = "flex";
     startScreen.style.display = "none";
 
 }
 
+// Function to reset the board
 function resetBoard(){
 
+    // Remove all markers and blink class from cells
     cells.forEach(cell => {
         cell.classList.remove(`${X_CLASS}`);
         cell.classList.remove(`${O_CLASS}`);
         cell.classList.remove('blink');
     })
 
+    // Start the game again
     startGame();
 }
 
+// Event listener for restart button
+restartButton.addEventListener("click", () => {
+    dialog.close();
+    location.reload();
+})
 
+// Function to handle cell click event
 function handleClickCell(e) {
-
+    // Get the clicked cell
     const cell = e.target;
 
+    // If the cell already has X or O class, or if it's AI's turn and the current player is not X, return
     if (cell.classList.contains(X_CLASS) || cell.classList.contains(O_CLASS) || (isOpponentAi && currentPlayer !== X_CLASS)) {
         return;
     }
 
+    // Place marker on the clicked cell
     placeMarker(cell, currentPlayer)
 
+    // Check if the current player has won
     if(checkWin(currentPlayer)){
+        // Handle score and remove click event listeners from all cells
         handleScore(currentPlayer);
         cells.forEach(cell => {
             cell.removeEventListener("click", handleClickCell);
         });
     } else if (isDraw()) {
+        // If it's a draw
+        // Reset the board after 2 seconds
         setTimeout(resetBoard, 2000);
     } else {
+        // If the game continues
+        // Swap turn
         swapTurn();
         if(isOpponentAi){
+            // If opponent is AI, wait for 1 second and let AI make a move
             setTimeout(aiMove, 1000);
         }else{
             return;
@@ -93,28 +120,12 @@ function handleClickCell(e) {
     }
 }
 
-function endGame() {
-
-    dialog.showModal();
-
-    if(currentPlayer === X_CLASS){
-        announce.textContent = `X's Win!`;
-    }else{
-        announce.textContent = `O's Win!`;
-    }
-    
-}
-
-restartButton.addEventListener("click", () => {
-    dialog.close();
-    location.reload();
-})
-
+// Function to handle score after a win
 function handleScore(currentPlayer){
-
+    // Select score elements
     const xScoreSpan = document.querySelector(".x-score");
     const oScoreSpan = document.querySelector(".o-score");
-
+    // Update scores based on current player
     if(currentPlayer === X_CLASS){
         xScore++;
         xScoreSpan.textContent = xScore;
@@ -133,26 +144,27 @@ function handleScore(currentPlayer){
         }
     }
 
-    
+    // Highlight winning combination cells
     const winningCombination = checkWin(currentPlayer);
-
     winningCombination.forEach(index => {
         cells[index].classList.add('blink');
     });
 
 }
 
+// Function to place marker on a cell
 function placeMarker(cell, currentPlayer){
     cell.classList.add(currentPlayer);
 }
 
-
+// Function to swap player turn
 function swapTurn(){
+    // Select X and O labels
     const xLabel = document.querySelector("#x_label");
     const oLabel = document.querySelector("#o_label");
-
+    // Change current player
     currentPlayer = currentPlayer == X_CLASS ? O_CLASS : X_CLASS;
-
+    // Toggle label borders based on current player
     if(currentPlayer == X_CLASS){
         oLabel.classList.remove("o_label_border");
         xLabel.classList.add("x_label_border");
@@ -162,6 +174,7 @@ function swapTurn(){
     }
 }
 
+// Function to check winning combination
 function checkWin(currentPlayer) {
     return winningCombos.find(combination => {
         return combination.every(index => {
@@ -170,14 +183,16 @@ function checkWin(currentPlayer) {
     });
 }
 
+// Function to check if it's a draw
 function isDraw() {
     return [...cells].every(cell => {
         return cell.classList.contains(X_CLASS) || cell.classList.contains(O_CLASS);
     });
 }
 
+// Function for AI move
 function aiMove() {
-
+    // Depending on difficulty, choose the appropriate move
     if (currentDifficulty === Difficulty.EASY) {
         makeRandomMove();
     } else if (currentDifficulty === Difficulty.UNBEATABLE) {
@@ -185,11 +200,15 @@ function aiMove() {
     }
 }
 
+// Function for AI's random move
 function makeRandomMove() {
-
+    // Filter empty cells
     let emptyCells = [...cells].filter(cell => !cell.classList.contains(X_CLASS) && !cell.classList.contains(O_CLASS));
+    // Get a random index from the array of empty cells
     let randomIndex = Math.floor(Math.random() * emptyCells.length);
+    // Get the random cell using the random index
     let randomCell = emptyCells[randomIndex];
+    // Place marker on the random cell
     placeMarker(randomCell, currentPlayer);
 
     if (checkWin(currentPlayer)) {
@@ -201,16 +220,23 @@ function makeRandomMove() {
     }
 }
 
-
+// Function for AI's move using Minimax algorithm
 function makeMinMaxMove() {
+    // Initialize variables to track the best score and the move
     let bestScore = -Infinity;
     let move;
 
+    // Loop through each cell
     cells.forEach((cell, index) => {
+        // Check if the cell is empty
         if (!cell.classList.contains(X_CLASS) && !cell.classList.contains(O_CLASS)) {
+            // Try placing the current player's marker on the cell
             cell.classList.add(currentPlayer);
+            // Use the Minimax algorithm to calculate the score for the move
             let score = minimax(cells, 0, false);
+            // Remove the marker from the cell to backtrack
             cell.classList.remove(currentPlayer);
+            // Update the best score and move if the current move is better
             if (score > bestScore) {
                 bestScore = score;
                 move = index;
@@ -218,7 +244,9 @@ function makeMinMaxMove() {
         }
     });
 
+    // Place the current player's marker on the cell with the best move
     cells[move].classList.add(currentPlayer);
+    // Remove the click event listener from the cell to prevent further interaction
     cells[move].removeEventListener("click", handleClickCell);
 
     if (checkWin(currentPlayer)) {
@@ -230,83 +258,114 @@ function makeMinMaxMove() {
     }
 }
 
+// Function for Minimax algorithm
 function minimax(cells, depth, isMaximizing) {
 
+    // Check for terminal states (win, lose, draw)
     if (checkWin(X_CLASS)) {
-        return -10;
+        return -10; // X wins
     } else if (checkWin(O_CLASS)) {
-        return 10;
+        return 10; // O wins
     } else if (isDraw()) {
-        return 0;
+        return 0; // Draw
     }
 
+    // Recursive search for the best move
     if (isMaximizing) {
         let bestScore = -Infinity;
         cells.forEach((cell) => {
             if (!cell.classList.contains(X_CLASS) && !cell.classList.contains(O_CLASS)) {
+                // Try placing O's marker
                 cell.classList.add(O_CLASS);
+                // Recursive call for minimizing player (X)
                 let score = minimax(cells, depth + 1, false);
+                // Undo the move
                 cell.classList.remove(O_CLASS);
+                // Update the best score
                 bestScore = Math.max(score, bestScore);
             }
         });
+        // Return the best score for maximizing player (O)
         return bestScore;
 
     } else {
         let bestScore = Infinity;
         cells.forEach((cell) => {
             if (!cell.classList.contains(X_CLASS) && !cell.classList.contains(O_CLASS)) {
+                // Try placing X's marker
                 cell.classList.add(X_CLASS);
+                // Recursive call for maximizing player (O)
                 let score = minimax(cells, depth + 1, true);
+                // Undo the move
                 cell.classList.remove(X_CLASS);
+                // Update the best score
                 bestScore = Math.min(score, bestScore);
             }
         });
+        // Return the best score for minimizing player (X)
         return bestScore;
     }
 
 }
 
-
-
+// Function to set up game based on opponent type
 function setupGame(state){
 
     isOpponentAi = state;
 
 }
 
+// Function to end the game and display the result
+function endGame() {
+    // Show the dialog modal
+    dialog.showModal();
+
+    // Set the announcement text based on the winner
+    if(currentPlayer === X_CLASS){
+        announce.textContent = `X's Win!`;
+    }else{
+        announce.textContent = `O's Win!`;
+    }
+    
+}
+
+// Event listener for human button
 humanButton.addEventListener("click", function() {
     setupGame(false);
-
+    // Highlight selected button
     this.style.backgroundColor = "white";
     this.style.color = "black";
     this.style.transform = "scale(1.12)";
-
+    // Reset style for AI button
     aiButton.style.backgroundColor = "";
     aiButton.style.color = "";
     aiButton.style.transform = "";
-
+    // Hide AI difficulty button
     aiDiffButton.style.display = "none";
 });
 
+// Event listener for AI button
 aiButton.addEventListener("click", function() {
     setupGame(true);
-
+    // Highlight selected button
     this.style.backgroundColor = "white";
     this.style.color = "black";
     this.style.transform = "scale(1.12)";
-
+    // Reset style for human button
     humanButton.style.backgroundColor = "";
     humanButton.style.color = "";
     humanButton.style.transform = "";
-
+    // Show AI difficulty button
     aiDiffButton.style.display = "block";
 });
 
+// Event listener for start button
 startButton.addEventListener("click", startGame);
 
+// Event listener for AI difficulty button
 aiDiffButton.addEventListener("click", toggleAIDifficulty);
 
+// Function to toggle AI difficulty
 function toggleAIDifficulty() {
 
     if (currentDifficulty === Difficulty.EASY) {
@@ -318,6 +377,7 @@ function toggleAIDifficulty() {
     }
 }
 
+// Highlight AI button on page load
 window.onload = () =>{
     aiButton.style.backgroundColor = "white";
     aiButton.style.color = "black";
